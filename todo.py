19826534aae447a8270123529243325d41d7fdbd
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2018-03-16 23:22:50 +0100

    Add some POST action with done task

--- todo.py
+++ todo.py
@@ -57,5 +57,16 @@ def zadania():
     return render_template('zadania_lista.html', zadania=zadania, error=error)
 
 
+@app.route('/zrobione', methods=['POST'])
+def zrobione():
+    """Zmiana statusu zadania na wykonane."""
+    zadanie_id = request.form['id']
+    db = get_db()
+    db.execute('UPDATE zadania SET zrobione=1 WHERE id=?', [zadanie_id])
+    db.commit()
+    flash('Zmieniono status zadania.')
+    return redirect(url_for('zadania'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
