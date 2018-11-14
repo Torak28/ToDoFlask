5c2b615f7a6fb4884badbd5babf9afd66e319b96
Jarosław Ciołek-Żelechowski <zelechowski28@gmail.com>
2018-11-14 21:40:53 +0100

    Just a start with resplus

---
+++ server.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request
+from flask_restplus import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+
+	def get(self):
+		"""Simple static get"""
+		return {'hello': 'world'}, 201
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5000)
